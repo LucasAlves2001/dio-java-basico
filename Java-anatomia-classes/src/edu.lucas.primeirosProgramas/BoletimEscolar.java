f2aebad22f005d5fa1a1569fecaa43f944d516c9
LucasAlves2001 <alves.antonio@aluno.ifsp.edu.br>
2023-09-16 20:15:25 -0300

    Novos Exercicios Feitos 16/09/23

---
+++ Java-anatomia-classes/src/edu.lucas.primeirosProgramas/BoletimEscolar.java
@@ -0,0 +1,12 @@
+public class BoletimEscolar {
+    public static void main(String[] args) {
+        int mediafinal = 10;
+        if(mediafinal<6)
+            System.out.println("REPROVADO");
+         else 
+            if(mediafinal == 6)
+                System.out.println("PROVA MINERVA");
+            else
+                    System.out.println("APROVADO");
+}
+}
---
+++ Java-anatomia-classes/src/edu.lucas.primeirosProgramas/Minha_Classe.java
@@ -0,0 +1,17 @@
+public class Minha_Classe {
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Lucas";
+        String segundoNome = "Antonio";
+        String terceiroNome = "Santos";
+        String quartoNome = "Alves";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome, quartoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome, String terceiroNome, String quartoNome){
+        return"Nome: " +  primeiroNome.concat(" ").concat(segundoNome).concat(" ").concat(terceiroNome).concat(" ").concat(quartoNome);
+    }
+}
